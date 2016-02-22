33dfb8136e300d5cda3f1479092ae8d8d96364fe
Rz Rasel <rzrasel@users.noreply.github.com>
2016-02-22 10:13:50 +0600

    Update Android Code One.java

--- Android Code One.java
+++ Android Code One.java
@@ -1,13 +1,13 @@
 |------------------------------------------------------------|
 public int getRandomInRange(int argMinVal, int argMaxVal) {
-    if (argMinVal > argMaxVal) {
-        int tempVal = argMaxVal;
-        argMaxVal = argMinVal;
-        argMinVal = tempVal;
-    }
-    Random random = new Random();
-    int retVal = random.nextInt(argMaxVal - argMinVal + 1) + argMinVal;
-    return retVal;
+	if (argMinVal > argMaxVal) {
+		int tempVal = argMaxVal;
+		argMaxVal = argMinVal;
+		argMinVal = tempVal;
+	}
+	Random random = new Random();
+	int retVal = random.nextInt(argMaxVal - argMinVal + 1) + argMinVal;
+	return retVal;
 }
 |------------------------------------------------------------|
 public List<Integer> getNumberToDigits(int argNumber)
